dba0529d3b716f3ce838536a8b68fc7ac14aeffb
Imogen Misso <imogenmisso@hotmail.co.uk>
2018-10-03 16:55:43 +0100

    step 16

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -30,7 +30,7 @@ describe DockingStation do
   end
 
   it 'doesnt accept more than 20 bikes' do
-    20.times {subject.dock(Bike.new)}
+    DockingStation::DEFAULT_CAPACITY.times {subject.dock(Bike.new)}
     expect {subject.dock(Bike.new)}.to raise_error 'Full capacity'
   end
 
