d5717eac80012ef4489603d374eecb9db7b0e2fe
Imogen Misso <imogenmisso@hotmail.co.uk>
2018-10-03 16:26:58 +0100

    step 14

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,15 +2,19 @@
 require_relative 'bike'
 
 class DockingStation
-  attr_reader :bike
+  attr_reader :bikes
+
+  def initialize
+    @bikes = []
+  end
 
   def release_bike
-    fail "no bikes" unless @bike
-    @bike
+    fail "no bikes" if @bikes.empty?
+    @bikes.pop
   end
 
   def dock(bike)
-    fail 'Full capacity' if @bike
-    @bike = bike
+    fail 'Full capacity' if @bikes.length >= 20
+    @bikes << bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -11,9 +11,9 @@ describe DockingStation do
   end
 
   it 'docking bike' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
+    bikes = Bike.new
+    subject.dock(bikes)
+    expect(subject.bikes).to eq [bikes]
   end
 
   it {is_expected.to respond_to(:dock).with(1).argument }
@@ -22,8 +22,15 @@ describe DockingStation do
     expect {subject.release_bike}.to raise_error "no bikes"
   end
 
-  it 'stop accepting bikes if its full' do
-    subject.dock(Bike.new)
+  it 'accepts more than one bike' do
+    bike_a = Bike.new
+    bike_b = Bike.new
+    subject.dock(bike_a)
+    expect(subject.dock(bike_b)).to eq [bike_a, bike_b]
+  end
+
+  it 'doesnt accept more than 20 bikes' do
+    20.times {subject.dock(Bike.new)}
     expect {subject.dock(Bike.new)}.to raise_error 'Full capacity'
   end
 
