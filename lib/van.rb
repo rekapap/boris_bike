32201ca0942258eb8bec0e9234f032bd52c065d2
Reka <rekapap28@gmail.com>
2018-10-04 17:13:24 +0100

    step 21

--- lib/van.rb
+++ lib/van.rb
@@ -1,9 +1,11 @@
 # Van
 class Van
   def deliver_bikes(bikes, garage)
-    
+    garage.dock(bikes)
   end
-  def collect(working_bikes, garage)
 
+  def distribute(working_bikes, station)
+    station.dock(working_bikes)
   end
+
 end
--- spec/vans_spec.rb
+++ spec/vans_spec.rb
@@ -5,11 +5,23 @@ describe Van do
 
   let(:bike) { double(:bike) }
   let(:garage) { double(:garage) }
+  let(:dockingstation) { double(:dockingstation) }
+  
   it 'delivers broken bikes to garage' do
     bikes = [bike, bike, bike]
+    allow(bike).to receive(:working?).and_return(false)
     allow(garage).to receive(:bikes).and_return(bikes)
+    allow(garage).to receive(:dock).and_return(bikes)
     subject.deliver_bikes(bikes, garage)
     expect(garage.bikes).to eq bikes
+  end
+
+  it 'distribute working bikes from garage' do
+    bikes = [bike, bike, bike]
+    allow(dockingstation).to receive(:bikes).and_return(bikes)
+    allow(dockingstation).to receive(:dock).and_return(bikes)
+    subject.distribute(bikes, dockingstation)
+    expect(dockingstation.bikes).to eq bikes
   end
 
 end
